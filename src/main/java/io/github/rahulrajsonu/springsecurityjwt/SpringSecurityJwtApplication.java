7f167c3b651951febcb6f9e6e4181550f04558d2
rahul.raj <rahul.raj@rahul.raj-mac>
2021-02-10 13:02:12 +0530

    initial code setup

---
+++ src/main/java/io/github/rahulrajsonu/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,14 @@
+package io.github.rahulrajsonu.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
+
